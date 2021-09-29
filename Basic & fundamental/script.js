2185bc44ea0f568115ac5b60378dd286e1604f7f
sohail49 <sohailahmadpsh@gmail.com>
2021-09-29 19:07:54 +0500

    What are Datatypes

---
+++ Basic & fundamental/script.js
@@ -0,0 +1,35 @@
+// what Are variables
+
+// let firstName;
+// firstName = 'john'
+// console.log(firstName);
+
+// const firstName = 'John';
+// console.log(firstName);
+
+// letter&number _ $ 
+// Cant Not Start a Name With number 
+
+// let firstName = 'John'; //camel Case
+// let first_name = 'John'; // snake case
+// let FirstName = 'John'; //pascal Case
+// let firstname = 'John'; //
+
+// Primitive datatype
+
+// let firstName = "John";
+// console.log(typeof firstName);
+// let year = 2021;
+// console.log(typeof year);
+// let isOld = false;
+// console.log(typeof isOld);
+// let lastName;
+// console.log(typeof lastName);
+// let cars = null;
+// console.log(typeof cars);
+
+// reference datatype
+
+let hobbies = ['coding' , 'gaming' , 123, true];
+
+console.log(typeof hobbies)
